5d25d7bbf233b5ce5aafb1c04947b247bf305dda
kai turanski <kai.turanski@mail.mcgill.ca>
2024-12-01 14:29:16 -0500

    Update BERT_eval_job.sh

--- jobs/BERT_eval_job.sh
+++ jobs/BERT_eval_job.sh
@@ -17,5 +17,5 @@ module load miniconda/miniconda-fall2024
 
 echo "BERT Test Job" > ../output/job-$job_number/test_out.txt
 nvidia-smi >> ../output/job-$job_number/test_out.txt
-python3 ../python/BERT_evaluation_script_2.py classification_head.pth #insert python script here
+python3 ../python/BERT_evaluation_script_2.py ../output/job-2824/7-epoch-head.pth #insert python script here
 
