4b58706a289330c702e3cb6f2e4d70926ed686ac
kai turanski <kai.turanski@mail.mcgill.ca>
2024-12-05 00:27:08 -0500

    Update BERT_param_job.sh

--- jobs/BERT_param_job.sh
+++ jobs/BERT_param_job.sh
@@ -17,5 +17,5 @@ module load miniconda/miniconda-fall2024
 
 echo "BERT Test Job" > ../output/job-$job_number/test_out.txt
 nvidia-smi >> ../output/job-$job_number/test_out.txt
-python3 ../python/BERT_experiments/over_hyperparam.py ../output/job-$job_number "head+1" "weight_decay" "0.01,0.1,0.2,0.3,0.4,0.5,0.6" #insert python script here
+python3 ../python/BERT_experiments/over_hyperparam.py ../output/job-$job_number "head+1" "batch_size" "8,16,32,64,128,256,512" #insert python script here
 
