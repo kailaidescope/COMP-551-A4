edd51310b830e34120e98e362db0a015391996b0
m3lodicaman <kai.turanski@mail.mcgill.ca>
2024-12-08 21:13:42 -0500

    Created local job

---
+++ jobs/local/BERT_epochs_job_win.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Access the job name
+job_number="local-$1"
+
+echo "BERT Test Job" > ../../output/win/job-$job_number/test_out.txt
+py -3.11 ../../python/BERT_experiments/over_epochs.py ../../output/win/job-$job_number "head+1" 8 #insert python script here
+
