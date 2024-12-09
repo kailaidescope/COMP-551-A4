834c4b873b60561dca52305fdb59d3cfbd267353
m3lodicaman <kai.turanski@mail.mcgill.ca>
2024-12-08 21:16:41 -0500

    Updated local job

--- jobs/local/BERT_epochs_job_win.sh
+++ jobs/local/BERT_epochs_job_win.sh
@@ -3,5 +3,5 @@
 job_number="local-$1"
 
 echo "BERT Test Job" > ../../output/win/job-$job_number/test_out.txt
-py -3.11 ../../python/BERT_experiments/over_epochs.py ../../output/win/job-$job_number "head+1" 8 #insert python script here
+py -3.11 ../../python/BERT_experiments/over_epochs.py ../../output/win/job-$job_number "head+1" 8 > job.out 2> job.err #insert python script here
 
